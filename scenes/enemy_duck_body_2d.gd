adc5a0c22444f75a091b8d866e3b2356c6b6a317
Juan Manuel Gelay <juanmanuelgelay@gmail.com>
2024-11-05 13:19:41 -0300

    Enemy duck collision to end game

--- scenes/enemy_duck_body_2d.gd
+++ scenes/enemy_duck_body_2d.gd
@@ -7,3 +7,8 @@ var animation_to_play = "Idle"
 func _ready():
 	animation_player.stop()
 	animation_player.play("Idle")
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if (body.name == "CharacterBody2D"):
+		print("You died.")
+		get_tree().reload_current_scene()
